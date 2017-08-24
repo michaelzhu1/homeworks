d5e3b054fb9c2a1473f47773e9a93a9b373fd5d3
Michael <mzhu105@gmail.com>
2017-08-24 00:27:13 -0700

    Add BCrypt gem and fix before_validation bug and SecureRandom syntax

--- W4D4/rails_auth/app/models/user.rb
+++ W4D4/rails_auth/app/models/user.rb
@@ -1,8 +1,10 @@
+require 'bcrypt'
+
 class User < ApplicationRecord
   validates :username, :session_token, presence: true, uniqueness:true
   validates :password_digest, presence: {message: 'Password can\'t be blank'}
   validates :password, length: {minimum: 6, allow_nil: true }
-  before validation :ensure_session_token
+  before_validation :ensure_session_token
 
   attr_reader :password
 
@@ -16,7 +18,7 @@ class User < ApplicationRecord
   end
 
   def self.generate_session_token
-    SecureRandom::urlsafe_64
+    SecureRandom::urlsafe_base64
   end
 
   def reset_session_token
